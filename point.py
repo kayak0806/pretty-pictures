8a70af321cd9e02d954838324b73fafdb48406bd
Kaitlin <kgallagher@Ember.(none)>
2012-11-13 14:44:46 -0500

    updated point

--- point.py
+++ point.py
@@ -1,14 +1,14 @@
 '''
 Creates points that stores
-	Cartesian position
-	Rotation position
-	Cartesian velocity
-	Rotation velocity
+    Cartesian position
+    Rotation position
+    Cartesian velocity
+    Rotation velocity
 '''
 class Point(object):
-	def __init__(self,car_position,rot_position,car_velocity,rot_position):
-		self.car_position=car_position
-		self.rot_position=rot_position
-		self.car_velocity=car_velocity
-		self.rot_velocity=rot_velocity	
+    def __init__(self,car_position,rot_position,car_velocity,rot_position):
+        self.car_position=car_position      # list [x,y,z]
+        self.rot_position=rot_position      # yaw or list [yaw, pitch, roll]
+        self.car_velocity=car_velocity      # list [x,y,z]
+        self.rot_velocity=rot_velocity	    # yaw or list [yaw, pitch, roll]
 		
