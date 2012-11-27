5f1b493254668f8b25bb867334de6c5fe01384d1
Kaitlin <kgallagher@Ember.(none)>
2012-11-27 13:18:24 -0500

    updated body

--- body.py
+++ body.py
@@ -1,6 +1,7 @@
 '''
 Creates a body that stores:
-    defining points
+    center point
+    size
     density
     mass
 '''
@@ -9,19 +10,34 @@ imporrt pyglet
 from point import Point
 
 class Body(pyglet.sprite.Sprite): # Using the Sprite class will let us animate objects with pyglet using Sprite.Update(). See: http://pyglet.org/doc/programming_guide/noisy.py
-    def __init__(self, def_points, density=1):
-        self.def_points=def_points
-        self.p = density
-        self.mass = self.area(def_points)
+    def __init__(self, center_point, size, shape, density=1):
+        self.center=center_point    # point
+        self.size = size            # list of sizes (eg. [radius] or [x, y])    
+        self.shape = shape          # string indicating shape (circle or rectangle for now)
+        self.p = density            # density, default 1
+        self.mass = self.area(size) * self.p # mass, based on area and density
 
 
-    def area(self,points):
-        a = 0
-        last = points[-1]
-        for point.car_position in Points:
-            print last
+class circleShape(Body):
+    def __init__(self, center_point, size, density=1):
+        self.center=center_point    # point
+        self.radius = size[0]       # radius
+        self.p = density            # density, default 1
+        self.mass = self.area(self.radius) * self.p # mass, based on area and density
+    
+    def area(self, raduis):
+        return r**2 * 3.14159
 
-
+class rectShape(Body):
+    def __init__(self, center_point, size, density=1):
+        self.center=center_point    # point
+        self.width = size[0]        # width
+        self.height = size[1]       # height
+        self.p = density            # density, default 1
+        self.mass = self.area(self.width, self.height) * self.p # mass, based on area and density
+    
+    def area(self, width, height):
+        return width * height
 
 p1 = Point([0,0],[0,0],[0,0],[0,0])
 p2 = Point([5,5],[0,0],[0,0],[0,0])
