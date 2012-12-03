a0bb3268594a0cde03745bf4c4392fe0816ac079
Louis Yi <louis78717@gmail.com>
2012-12-02 22:58:18 -0500

    swinging stuff

--- pyglet_stuff/animateSolidBounce.py
+++ pyglet_stuff/animateSolidBounce.py
@@ -1,6 +1,7 @@
 import pyglet
+from math import *
 
-window = pyglet.window.Window(width=400,height=400)
+window = pyglet.window.Window(width=500,height=400)
 
 class Environment(object):
     def __init__(self):
@@ -10,6 +11,7 @@ class Environment(object):
 environment = Environment()
         
 class body(object):
+    gravity=9.81
     
     def __init__(self,environment, points = (0,0),color = None):
         if color == None:
@@ -21,8 +23,10 @@ class body(object):
                 pyglet.gl.GL_POLYGON, None,('v2f',points),('c3B',color)
             )
         self.points=len(self.vertex_list.vertices)
-        self.dx=5
-        self.dy=5
+        self.dx=0
+        self.dy=0
+        self.centerMassX=55
+        self.centerMassY=105
         self.changeX=False
         self.changeY=False
         
@@ -30,23 +34,34 @@ class body(object):
         self.vertex_list.draw(arg)
         
     def update(self,dt):
-		for i in range(len(self.vertex_list.vertices)):
-			if i%2==0 and (self.vertex_list.vertices[i]<0 or self.vertex_list.vertices[i]>400):
-				self.changeX=True
-			if i%2==1 and (self.vertex_list.vertices[i]<0 or self.vertex_list.vertices[i]>400):
-				self.changeY=True
-		if self.changeX==True:
-			self.dx=self.dx*-1
-			self.changeX=False
-		if self.changeY==True:
-			self.dy=self.dy*-1
-			self.changeY=False
-		for i in range(len(self.vertex_list.vertices)):
-			if i%2==0:
-				self.vertex_list.vertices[i] = self.vertex_list.vertices[i] + self.dx
-			if i%2==1:
-				self.vertex_list.vertices[i] = self.vertex_list.vertices[i] + self.dy
-
+        if self.fixPointX!=None:
+            self.dx=self.dx+9.81*atan((self.fixPointX-self.centerMassX)/(self.fixPointY-self.centerMassY))*dt
+            print self.dx
+        for i in range(len(self.vertex_list.vertices)):
+            if i%2==0 and (self.vertex_list.vertices[i]<0 or self.vertex_list.vertices[i]>500):
+                self.changeX=True
+            if i%2==1 and (self.vertex_list.vertices[i]<0 or self.vertex_list.vertices[i]>400):
+                self.changeY=True
+        if self.changeX==True:
+            self.dx=self.dx*-1
+            self.changeX=False
+        if self.changeY==True:
+            self.dy=self.dy*-1
+            self.changeY=False
+        for i in range(len(self.vertex_list.vertices)):
+            if i%2==0:
+                self.vertex_list.vertices[i] = self.vertex_list.vertices[i] + self.dx
+            if i%2==1:
+                self.vertex_list.vertices[i] = self.vertex_list.vertices[i] + self.dy
+        self.centerMassX+=self.dx
+        self.centerMassY+=self.dy
+         
+    def fixPoint(self,x,y):
+        self.fixPointX=x
+        self.fixPointY=y
+        
+        
+        
 @window.event
 def on_draw():
     window.clear()
@@ -56,20 +71,18 @@ def update(dt):
     for obj in environment.obj_ls:
         obj.update(dt)
 
-dt = 1/30.
-pyglet.clock.schedule_interval(update, dt) # Schedules updates for all objects every 30th of a second (Float)
+dt = 1/60.
+pyglet.clock.schedule_interval(update, dt) # Schedules updates for all objects every 60th of a second (Float)
 
-body1 = body(environment,(10,15,30,25,30,10),
+body1 = body(environment,(50,100,50,110,60,110,60,100),
             (0,0,255,
+            0,0,255,
             0,0,255,
             0,0,255)
             )
+body1.fixPoint(100,150)
 environment.obj_ls.append(body1)
 
-#vertex_list = environment.obj_batch.add(2, pyglet.gl.GL_POINTS, None,
-#    ('v2i', (10, 15, 30, 35)),
-#    ('c3B', (0, 0, 255, 0, 255, 0))
-#)
 
 
 if __name__ == "__main__":
