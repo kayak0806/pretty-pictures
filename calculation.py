7dee5ca6ffbbe21605fe7b05dd647f869294eb5a
Louis Yi <louis78717@gmail.com>
2012-12-12 19:08:45 -0500

    yay

---
+++ calculation.py
@@ -0,0 +1,16 @@
+from numpy import *
+
+def gravity(center,velocity):
+    return (0,-9.81)
+
+def Centrp(center,velocity,fixLength,fixCenter):
+    vectorHor=array([1,0])
+    vectorVer=array([0,1])
+    vectorTan=array([-(fixCenter[1]-center[1]),fixCenter[0]-center[0]])
+    vectorNor=array([fixCenter[0]-center[0],fixCenter[1]-center[1]])
+    vectorTan=vectorTan/linalg.norm(vectorTan)
+    vectorNor=vectorNor/linalg.norm(vectorNor)
+    CentrpAccel=(velocity[0]**2+velocity[1]**2)/fixLength
+    ddx=CentrpAccel*dot(vectorHor,vectorNor)+9.81*dot(vectorVer,vectorNor)*dot(vectorHor,vectorNor)
+    ddy=CentrpAccel*dot(vectorVer,vectorNor)+9.81*dot(vectorVer,vectorNor)*dot(vectorVer,vectorNor)
+    return (ddx,ddy)
